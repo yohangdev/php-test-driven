dbdf5460233dcc5d21d438a64714fc724a9e6672
Yoga Hanggara <yohang88@gmail.com>
2019-01-17 01:15:17 +0700

    refactor if without else for clean code

--- src/LetterService.php
+++ src/LetterService.php
@@ -5,21 +5,30 @@ class LetterService
     public function generateNumber($year, $letter, $latest)
     {
         if ($latest === null) {
-            $number = 0;
-        } else {
-            preg_match('/(\d*)-(.?)-(\d*)/', $latest, $output_array);
+            $number = 1;
 
-            $latestYear = (int) $output_array[1];
-
-            if ($latestYear === $year) {
-                $number = (int) $output_array[3];
-            } else {
-                $number = 0;
-            }
+            return $this->formatNumber($year, $letter, $number);
         }
 
-        $number++;
+        preg_match('/(\d*)-(.?)-(\d*)/', $latest, $output_array);
 
+        $lastYear = (int) $output_array[1];
+
+        if ($lastYear === $year) {
+            $number = (int) $output_array[3];
+
+            $number++;
+
+            return $this->formatNumber($year, $letter, $number);
+        }
+
+        $number = 1;
+
+        return $this->formatNumber($year, $letter, $number);
+    }
+
+    protected function formatNumber($year, $letter, $number)
+    {
         return sprintf('%s-%s-%s', $year, $letter, str_pad($number, 4, '0', STR_PAD_LEFT));
     }
 }
